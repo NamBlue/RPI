98c48e64d787bbbe8f5117d6c5a565a4ffee131e
George <namblue@me.com>
2016-06-12 08:59:48 +0000

    inital commit

---
+++ RPI/bin/Debug/main.d
@@ -0,0 +1,2 @@
+/home/pi/Projects/RPI/RPI/bin/Debug/main.o: \
+ /home/pi/Projects/RPI/RPI/main.c
---
+++ RPI/main.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <wiringPi.h>
+#define DELAY 30
+
+
+int main (int argc, char *argv[]) {
+	size_t x = 0;
+
+	wiringPiSetupGpio();
+
+	pinMode(18, OUTPUT);
+	pinMode(17, OUTPUT);
+
+	printf ("Hello world!\n");
+	while(x != 50) {
+		digitalWrite(18, HIGH);
+		delay(DELAY);
+		digitalWrite(18, LOW);
+		delay(DELAY);
+		digitalWrite(17, HIGH);
+		delay(DELAY);
+		digitalWrite(17, LOW);
+		delay(DELAY);
+		x++;
+	}
+	getchar();
+	return 0;
+}
+

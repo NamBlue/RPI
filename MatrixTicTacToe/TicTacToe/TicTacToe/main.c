a582072704acfb9bd984c2e9931f780ee77fe536
George <namblue@me.com>
2016-07-24 00:11:13 +0000

    added getCurrentTime function and gameloop foundation, also added debugging print lines

--- MatrixTicTacToe/TicTacToe/TicTacToe/main.c
+++ MatrixTicTacToe/TicTacToe/TicTacToe/main.c
@@ -1,6 +1,9 @@
 ﻿#include <stdio.h>
+#include <time.h>
 #include <wiringPi.h>
 #define LINESIZE 256
+/* 1000MS per Second so 1000MS/60FPS = 16.7MS_PER_UPDATE */
+#define MS_PER_UPDATE 16
 
 int main (int argc, char *argv[])
 {
@@ -17,23 +20,12 @@ int setup(void) {
 
 	wiringPiSetupGpio();
    
-   for (size_t i = 1; i < 4; i++)
-      {
-      printf("Please enter the GPIO pin number for Column %d: \n", i);
-      col1 = getUserInt();
-      pinMode(col1, OUTPUT);
-
-      printf("Please enter the GPIO pin number for Row %d: \n", i);
-	col1 = getUserInt();
-	pinMode(col1, OUTPUT);
-      col1 = getUserInt();
-      pinMode(col1, OUTPUT);
-      }
 
 	return 0;	
 }
 
-int getUserInt(void) {
+int getUserInt(void) 
+{
 	char input[LINESIZE];
 	int output = 0;
 
@@ -50,16 +42,69 @@ int getUserInt(void) {
 	}
 }
 
-int run(void) {
-	int exit = 0;
-	while (exit != 1) {
+void processInput(void) 
+{
+	return;
+}
 
-	printf("\nEnter 1 to exit>");
-	exit = getUserInt();
+void update(void) 
+{
+	return;
+}
+
+void render(void) 
+{
+	return;
+}
+
+long getCurrentTime(void) 
+{
+	int timeout = 0;
+	clock_t current_time;
+
+	current_time = clock();
+
+	while (current_time == ((time_t)-1) && (timeout <= 2000))
+	{		
+		printf("\nError obtaining the current time, retrying..");
+		delay(10);
+		timeout += 10;
+	}
+
+	long time = current_time;
+	return time;	
+}
+
+int run(void) 
+{
+	int exit = 0;
+	long previous = getCurrentTime();
+	long lag = 0;
+
+	while (exit < 500) 
+	{
+		long current = getCurrentTime();
+		long elapsed = current - previous;
+		previous = current;
+		/* time is in 1/10 of a millisecond so need to divide be 10 to get miliseconds */
+		lag += (elapsed / 10);
+
+		processInput();
+
+		printf("\nLag = %d \n Current Time = %d", lag , current/10);
+		while(lag >= MS_PER_UPDATE) 
+		{
+			update();
+			lag -= MS_PER_UPDATE;
+		}
+
+		exit++;
+		render();
 	}
 	return 0;
 }
 
-int end(void) {
+int end(void) 
+{
 	return 0;
 }
